423501f3d298d90fb4e28be1ffbf6f6dc9a647b3
kremlevmax <kremlevmax1989@gmail.com>
2022-09-29 03:21:16 -0700

    Add Basic Mechanics

--- src/App.js
+++ src/App.js
@@ -6,32 +6,21 @@ import StartButton from "./components/StartButton/StartButton";
 
 const url = "http://localhost:4000/api/v1/questions/";
 
-const fetchQuestion = async () => {
-  try {
-    const data = await axios(url, {
-      headers: { Accept: "application/json" },
-      body: {
-        askedIds: [
-          "632fca8250c2a50234bf353f",
-          "632fca8250c2a50234bf353e",
-          "632fca8250c2a50234bf3540",
-          "632fca8250c2a50234bf3541",
-          "632fca8250c2a50234bf3542",
-          "632fca8250c2a50234bf3543",
-          "632fca8250c2a50234bf3544",
-        ],
-      },
-    });
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 function App() {
   const [isStarted, setIsStarted] = useState(false);
   const [askedIds, setAskedIds] = useState([]);
   const [question, setQuestion] = useState("");
+
+  const fetchQuestion = async () => {
+    try {
+      const { data } = await axios(url, {
+        params: {
+          askedIds: askedIds,
+        },
+      });
+      setQuestion(data);
+    } catch (error) {}
+  };
 
   useEffect(() => {
     fetchQuestion();
@@ -41,7 +30,11 @@ function App() {
     <div className='App'>
       <div className='container'>
         {isStarted ? (
-          <Card />
+          <Card
+            question={question}
+            askedIds={askedIds}
+            setAskedIds={setAskedIds}
+          />
         ) : (
           <StartButton onClickHandler={() => setIsStarted(true)} />
         )}
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "./Card.module.css";
 
-const Card = () => {
+const Card = ({ question, setAskedIds, askedIds }) => {
   const [cardClasses, setCardClasses] = useState(styles.card);
 
   const onClickHandler = () => {
@@ -15,8 +15,19 @@ const Card = () => {
   return (
     <div className={cardClasses}>
       <div className={styles.card__inner} onClick={onClickHandler}>
-        <div className={styles.card__front}>Test Question</div>
-        <div className={styles.card__back}>Test Answer</div>
+        <div className={styles.card__front}>
+          <span>{question.question.question}</span>
+        </div>
+        <div className={styles.card__back}>
+          <span>{question.question.answer}</span>
+          <div className={styles.card__none}>No</div>
+          <div
+            className={styles.card__yes}
+            onClick={() => setAskedIds([...askedIds, question.question._id])}
+          >
+            Yes
+          </div>
+        </div>
       </div>
     </div>
   );
